a8fa436013482345cb3eb7a8c3eaa709be6843e9
ongsababa123 <jailyootbandit@gmail.com>
2021-09-30 15:17:42 +0700

    C++ LAB5

---
+++ lab 5/lab5-8EX.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int power2(int); /*  function prototype  */
+int main()
+{
+    int  x = 1;
+    while ( x <= 10)
+        {
+        cout << power2(x) << endl; /* calling function */
+        x = x + 1;
+        }
+        return(0);
+}
+
+    int power2(int n)  /*  Definition Function*/
+    {
+        return(n * n);
+        }
