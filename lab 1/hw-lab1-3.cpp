992d57fe6875d36899be0f72a332ef7a57a18f2b
ongsababa123 <75301993+ongsababa123@users.noreply.github.com>
2021-09-30 15:20:09 +0700

    LAB1
    LAB1 C++

---
+++ lab 1/hw-lab1-3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    std::cout << "Student Cod  : 64-060214-2117-1"<< endl;
+    std::cout << "Student Name : Jirayut Bandit"<< endl;
+    std::cout << "Division     : Information Technology"<< endl;
+    std::cout << "Year         : 1"<< endl;
+    std::cout << "Room         : RB"<< endl;
+    std::cout << "Sex          : Male"<< endl;
+    std::cout << "Age          : 20"<< endl;
+    return(0);
+}
---
+++ lab 1/hw-lab1-4.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    std::cout << "#####        #      #         #   #   #         "<< endl;
+    std::cout << "#    #     #   #    # #       #   #  #          "<< endl;
+    std::cout << "#     #   #     #   #   #     #   # #           "<< endl;
+    std::cout << "######    #     #   #     #   #   #  #          "<< endl;
+    std::cout << "#    #    #######   #       # #   #   #         "<< endl;
+    std::cout << "#####     #     #   #         #   #    #        "<< endl;
+    return(0);
+}
